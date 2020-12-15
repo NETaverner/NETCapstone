2d0bb3809dcb793d9e2e8e3910dc3b3d567df1c7
NETaverner <netaverner@gmail.com>
2020-12-15 13:06:13 +0200

    Final

--- FinalProject.R
+++ FinalProject.R
@@ -1,4 +1,4 @@
-#Librarys
+#Libraries
 if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
 if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
 if(!require(corrplot)) install.packages("recorrplotadr", repos = "http://cran.us.r-project.org")
@@ -15,33 +15,20 @@ if(!require(glmnet)) install.packages("glmnet", repos = "http://cran.us.r-projec
 if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
 if(!require(nnet)) install.packages("nnet", repos = "http://cran.us.r-project.org")
 if(!require(funModeling)) install.packages("funModeling", repos = "http://cran.us.r-project.org")
-if(!require(Momocs)) install.packages("Momocs", repos = "http://cran.us.r-project.org")
+#if(!require(Momocs)) install.packages("Momocs", repos = "http://cran.us.r-project.org")
+#if(!require(klaR)) install.packages("klaR", repos = "http://cran.us.r-project.org")
+
 
 # The data file will be loaded from my personal github account
-data <- read.csv("/Users/user/Documents/DataScience/FinalCapstone/NETCapstone/DataFiles/adult.csv")
+data <- read.csv("/Users/user/Documents/DataScience/FinalCapstone/NETCapstone/DataFiles/column_2C_weka.csv")
 
 # attributes 
-# Income 
+#Class
 
-#Features 
-#Age 
-#WorkingClass
-#fnlwgt
-#education
-#education.num
-#marital.status
-#occupation
-#relationship
-#race
-#sex
-#capital gain
-#capital loss
-#hour.per.week
-#native.country
+# 6 Features 
 
 
 #Data Analysis
-#not looking at capital gain, capital loss, hour.per.week
 
 str(data)
 
@@ -51,3 +38,322 @@ summary(data)
 
 #check for NA values
 map(data, function(.x) sum(is.na(.x)))
+
+#check proportions of income groups
+prop.table(table(data$class))
+
+#plot proportion
+options(repr.plot.width=4, repr.plot.height=4)
+ggplot(data, aes(x=class))+geom_bar(fill="blue",alpha=0.5)+theme_bw()+labs(title="Distribution of Class")
+
+#The most variables in the dataset are normally distributed as shown in the below plot, execpt for degree_spondylolisthesis:
+
+data %>% plot_num(bins=10) 
+
+#Check correlation
+
+correlationMatrix <- cor(data[,0:6])
+corrplot(correlationMatrix, order = "hclust", tl.cex = 1, addrect = 3)
+
+#As seen in plot there seems to be no variables that are highly correlated with each others, cor >= 0.9
+#due to this we can assume that methods that usually fail due to high correlation variable shouldn't be impacted on badly by the current variables
+#The Caret R package provides the findCorrelation which will analyze a correlation matrix of your data’s attributes report on attributes that can be removed. 
+#Because of much correlation some machine learning models could fail.
+#Following method below proves the assumption of no highly correlated variables
+
+# find Variables that are highly corrected (>0.90)
+highlyCorrelated <- findCorrelation(correlationMatrix, cutoff=0.7)
+# print indexes of highly correlated attributes
+print(highlyCorrelated)
+#There are no variables
+
+#Selecting the right features in our data can mean the difference between mediocre performance with long training times 
+#and great performance with short training times.
+
+# Remove correlated variables
+data2 <- data %>% select(!highlyCorrelated)
+# number of columns after removing correlated variables
+ncol(data2)
+
+#Lost 1 value
+
+#MODELING Approach
+
+#PCA
+PcaData <- prcomp(data[,0:6], center = TRUE, scale = TRUE)
+plot(PcaData, type="l")
+
+#summary
+summary(PcaData)
+
+# only need the first component to explain 0.541 of the variance, after applying 4 principle components 0.94566 of the variance can be explained. According to the 
+# summary above 1 of the variance can be explained after 5 principle components which 
+
+#plot of PC1 vs PC2
+pcaDf <- as.data.frame(PcaData$x)
+ggplot(pcaDf, aes(x=PC1, y=PC2, col=data$class)) + geom_point(alpha=0.5)
+#Kind of easy to separate into two classes
+
+
+#plot of densitys
+pc1 <- ggplot(pcaDf, aes(x=PC1, fill=data$class)) + geom_density(alpha=0.25)  
+pc2 <- ggplot(pcaDf, aes(x=PC2, fill=data$class)) + geom_density(alpha=0.25)  
+
+grid.arrange(pc1, pc2, ncol=2)
+
+#NEW
+
+pca_res_data2 <- prcomp(data2[,0:5], center = TRUE, scale = TRUE)
+plot(pca_res_data2, type="l")
+
+
+summary(pca_res_data2)
+
+#The above table shows that 95% of the variance is explained with 4 PC's in the transformed dataset data2.
+
+pca_df <- as.data.frame(pca_res_data2$x)
+ggplot(pca_df, aes(x=PC1, y=PC2, col=data$class)) + geom_point(alpha=0.5)
+
+#The data of the first 2 components can be easly separated into two classes. This is caused by the fact that the variance explained by these components is not large. The data can be easly separated.
+
+
+g_pc1 <- ggplot(pca_df, aes(x=PC1, fill=data$class)) + geom_density(alpha=0.25)  
+g_pc2 <- ggplot(pca_df, aes(x=PC2, fill=data$class)) + geom_density(alpha=0.25)  
+grid.arrange(g_pc1, g_pc2, ncol=2)
+
+
+
+#Linear Discriminant Analysis (LDA)
+
+LdaData <- MASS::lda(class~., data = data, center = TRUE, scale = TRUE) 
+LdaData
+#Data frame of the LDA for visualization purposes
+ldaDataPredict <- predict(LdaData, data)$x %>% as.data.frame() %>% cbind(class=data$class)
+
+
+#Plot density of LD1
+ggplot(lda_df_predict, aes(x=LD1, fill=class)) + geom_density(alpha=0.5)
+
+
+# Methods
+
+#creating train and training/validation datasets
+
+set.seed(1, sample.kind="Rounding") #if using R 3.5 or earlier, use `set.seed(1)
+#Train (80% of data)
+data3 <- cbind (class=data$class, data2)
+datSamplingIndex <- createDataPartition(data$class, times=1, p=0.8, list = FALSE)
+trainData <- data3[datSamplingIndex, ]
+
+#Test (20%)
+
+testData <- data3[-datSamplingIndex, ]
+
+#Control the data
+
+fitControl <- trainControl(method="cv",    #Control the computational nuances of thetrainfunction
+                           number = 20,    #Either the number of folds or number of resampling iterations
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary)
+# Naive Bayes Model
+
+
+NaiveBayesModel <- train(class~.,
+                      trainData,
+                      method="nb",
+                      metric="ROC",
+                      preProcess=c('center', 'scale'), #in order to normalize the data
+                      trace=FALSE, 
+                      importance = TRUE,
+                      trControl=fitControl)
+
+NBPrediction <- predict(NaiveBayesModel, testData)
+NBConfMatrix <- confusionMatrix(NBPrediction, as.factor(testData$class), positive = "Abnormal")
+NBConfMatrix
+
+
+#The most important variables that permit the best prediction and contribute the most to the model are the following:
+
+#ERORRRRRRRRRRRRRRRR
+
+###plot(varImp(NaiveBayesModel)) Doesn't work
+
+# Random Forest
+
+
+RandomforestModel <- train(class~.,
+                            trainData,
+                            method="rf",  #also recommended ranger, because it is a lot faster than original randomForest (rf)
+                            metric="ROC",
+                            #tuneLength=10,
+                            #tuneGrid = expand.grid(mtry = c(2, 3, 6)),
+                            preProcess = c('center', 'scale'),
+                            trControl=fitControl)
+
+RFPrediction <- predict(RandomforestModel, testData)
+#Check results
+RFConfMatrix <- confusionMatrix(RFPrediction, as.factor(testData$class), positive = "Abnormal")
+RFConfMatrix
+
+#plot importance
+varImp(RandomforestModel)
+
+plot(varImp(RandomforestModel), main="Top variables- Random Forest")
+
+
+
+# fit12_rf <- train(class ~., 
+#                   data = trainData,
+#                   method = "rf", 
+#                   tuneGrid = data.frame(mtry = seq(1, 7)), 
+#                   ntree = 100)
+# fit12_rf$bestTune
+# survived_hat <- predict(fit12_rf, testData)
+# mean(survived_hat == testData$class)
+# varImp(fit12_rf)
+
+
+# Logistic Regression Model 
+
+LogRegModel<- train(class~., data = trainData, 
+                     method = "glm",
+                     metric = "ROC",
+                     preProcess = c("scale", "center"),  # in order to normalize the data
+                     trControl= fitControl)
+
+LogRegPred <- predict(LogRegModel, testData)
+# Check results
+LogRegConfMatrix <- confusionMatrix(LogRegPred, as.factor(testData$class), positive = "Abnormal")
+LogRegConfMatrix
+
+#plot of importance
+
+plot(varImp(LogRegModel), main="Top variables - Log Regr")
+
+
+
+# K Nearest Neighbor (KNN) Model
+
+KNNModel <- train(class~.,
+                   trainData,
+                   method="knn",
+                   metric="ROC",
+                   preProcess = c('center', 'scale'),
+                   tuneLength=10, #The tuneLength parameter tells the algorithm to try different default values for the main parameter
+                   #In this case we used 10 default values
+                   trControl=fitControl)
+
+KNNPred <- predict(KNNModel, testData)
+KNNConfMatrix <- confusionMatrix(KNNPred, as.factor(testData$class), positive = "Abnormal")
+KNNConfMatrix
+
+#plot importance
+
+#plot(varImp(KNNModel),main="Top variables - KNN") Doesn't work
+
+#Plot the kNN model to investigate the relationship between the number of neighbors and accuracy on the training set.
+
+ggplot(KNNModel)
+
+RMSE(KNNPred,testData$class)
+
+
+# TrainDatas <- data[,1:6]
+# TrainClasses <- data[,6]
+# 
+# knnFit <- train(TrainDatas, TrainClasses, "knn")
+# 
+# knnImp <- varImp(knnFit)
+
+#dotPlot(knnImp)
+
+
+# Neural Network with LDA Model
+
+#test and training data
+trainDataLda <- ldaDataPredict[datSamplingIndex, ]
+testDataLda <- ldaDataPredict[-datSamplingIndex, ]
+
+# Model
+NNLDAModel <- train(class~., 
+                 trainDataLda,
+                 method="nnet",
+                 metric="ROC",
+                 preProcess=c('center', 'scale'),
+                 tuneLength=10,
+                 trace=FALSE,
+                 trControl=fitControl)
+
+NNLdaPred <- predict(NNLDAModel, testDataLda)
+NNLdaConfMatrix <- confusionMatrix(NNLdaPred, as.factor(testDataLda$class), positive = "Abnormal")
+NNLdaConfMatrix
+
+
+
+
+# Neural Network with PCA Model
+
+NNPCAModel <- train(class~.,
+                        trainData,
+                        method="nnet",
+                        metric="ROC",
+                        preProcess=c('center', 'scale', 'pca'),
+                        tuneLength=10,
+                        trace=FALSE,
+                        trControl=fitControl)
+
+NNPcaPred <- predict(NNPCAModel, testData)
+NNPcaConfMatrix <- confusionMatrix(NNPcaPred, as.factor(testData$class), positive = "Abnormal")
+NNPcaConfMatrix
+
+plot(varImp(NNPCAModel), main="Top variables - NNET PCA")
+
+#Results
+# comparisons
+
+models <- list(Naive_Bayes = NaiveBayesModel, 
+                    Logistic_regr = LogRegModel,
+                    Random_Forest = RandomforestModel,
+                    KNN = KNNModel,
+                    Neural_PCA = NNPCAModel,
+                    Neural_LDA = NNLDAModel)   
+
+modelsResults <- resamples(models)
+summary(modelsResults)
+
+#Plot variablities
+bwplot(modelsResults, metric="ROC")
+
+
+
+
+confMatrixs <- list(
+  Naive_Bayes = NBConfMatrix, 
+  Logistic_regr = LogRegConfMatrix,
+  Random_Forest = RFConfMatrix,
+  KNN = KNNConfMatrix,
+  Neural_PCA = NNPcaConfMatrix,
+  Neural_LDA = NNPcaConfMatrix)   
+
+ConfMatrixResults <- sapply(confMatrixs, function(x) x$byClass)
+ConfMatrixResults %>% knitr::kable()
+
+
+#discussion 
+
+MaxConfMatrix <- apply(ConfMatrixResults, 1, which.is.max)
+output_report <- data.frame(metric=names(MaxConfMatrix), 
+                            best_model=colnames(ConfMatrixResults)[MaxConfMatrix],
+                            value=mapply(function(x,y) {ConfMatrixResults[x,y]}, 
+                                         names(MaxConfMatrix), 
+                                         MaxConfMatrix))
+rownames(output_report) <- NULL
+output_report
+
+
+
+# Appendix - Environment
+
+print("Operating System:")
+version
+
